0ebc5cfc037bcef979f6e37c752efaeec09f0a1a
charleenmperrier <charlee_perrier@hotmail.com>
2021-03-16 12:57:24 -0700

    new folder for appointemnts component

--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,7 @@ import DayListItem from 'components/DayListItem';
 import DayList from 'components/DayList';
 import InterviewerListItem from 'components/InterviewerListItem';
 import InterviewerList from 'components/InterviewerList';
+import Appointment from 'components/Appointment/index.js';
 
 
 storiesOf("Button", module)
@@ -130,3 +131,14 @@ storiesOf("Button", module)
             setInterviewer={action("setInterviewer")}
           />
         ));
+
+
+        // appointments stories 
+      storiesOf("Appointment", module)
+        .addParameters({
+          backgrounds: [{name: "white", value: "#fff", default: true}]
+        })
+        .add("Appointment", () => <Appointment />)
+        .add("Appointment with Time", () => <Appointment time="12pm"/>);
+
+          
