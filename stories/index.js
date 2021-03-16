8915d67d23cc0ebd9ad6b8f8160609ed062a4757
charleenmperrier <charlee_perrier@hotmail.com>
2021-03-16 14:57:50 -0700

    error msg

--- stories/index.js
+++ stories/index.js
@@ -16,6 +16,7 @@ import Empty from 'components/Appointment/Empty';
 import Show from 'components/Appointment/Show';
 import Confirm from 'components/Appointment/Confirm';
 import Status from 'components/Appointment/Status';
+import Error from 'components/Appointment/Error';
 
 
 storiesOf("Button", module)
@@ -168,8 +169,19 @@ storiesOf("Button", module)
             onCancel={action("onCancel")} 
           />)
 
-        .add("Status", () => 
+        .add("Deleting", () => 
           <Status message="Deleting" />)
+        .add("Saving", () => 
+          <Status message="Saving" />)
+
+        .add("Error Deleting", () => 
+        <Error message="Could not delete appointment" 
+          onClose={action('onClose')}
+        />)
+        .add("Error Saving", () => 
+        <Error message="Could not save appointment" 
+          onClose={action('onClose')}
+        />)
 
 
           
