73d9a921905a607976d7d7fed9d3932503aec951
charleenmperrier <charlee_perrier@hotmail.com>
2021-03-16 08:39:13 -0700

    catching up on commits

---
+++ src/components/DayList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import 'components/DayListItem.scss'
+// import classnames from 'classnames'
+import DayListItem from 'components/DayListItem'
+
+export default function DayList(props) {
+  
+  const DayList = props.days.map((day, index) => {
+    return <DayListItem 
+      key={index} 
+      name={day.name} 
+      spots={day.spots} 
+      selected={day.name === props.day} 
+      setDay={props.setDay} />
+  })
+
+  return (
+    <ul>
+    {DayList}
+    </ul>
+  );
+}
---
+++ src/components/DayListItem.js
@@ -0,0 +1,17 @@
+import React from "react";
+import 'components/DayListItem.scss'
+import classnames from 'classnames'
+
+export default function DayListItem(props) {
+
+  const formatSpots = () => 
+  (props.spots ? `${props.spots} spot${props.spots > 1 ? `s` : ``} remaining` : `no spots remaining`)
+  
+  const dayClass = classnames("day-list__item", {'day-list__item--selected': props.selected}, {'day-list__item--full': props.spots === 0})
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 >{props.name}</h2> 
+      <h3 >{formatSpots()}</h3>
+    </li>
+  );
+}
