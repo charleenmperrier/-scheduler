78b0cdfd2fe1a664c44a21973c37244fdd54fce0
charleenmperrier <charlee_perrier@hotmail.com>
2021-03-16 13:26:17 -0700

    added more component and testing

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import 'components/Appointment/styles.scss'
-import classnames from 'classnames'
+// import classnames from 'classnames'
+
 
 export default function Appointment(props) {
 
   return (
-    <article className='appointment'></article>
+    <article className='appointment'>
+     
+    </article>
   );
 }
