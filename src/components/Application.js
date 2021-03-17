e42730e6012e5412b6d47756f286e3b06c086f65
charleenmperrier <charlee_perrier@hotmail.com>
2021-03-17 15:31:14 -0700

    update

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,27 +1,10 @@
 // import React from "react";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 import "components/Application.scss";
 import DayList from "components/DayList.js"
 import Appointment from "components/Appointment"
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = [
   {
@@ -61,7 +44,15 @@ const appointments = [
 
 export default function Application(props) {
 
-  const [day, setDay] = useState("Monday");
+  const [day, setDay] = useState([]);
+
+  useEffect(() => {
+    const daysUrl = `/api/days`
+    axios.get(daysUrl).then((response) => {
+      console.log(response.data);
+      setDay(response.data);
+    });
+  }, [])
 
   const appointmentList = appointments.map((appointment) => {
     return <Appointment key={appointment.id} {...appointment}/>
@@ -78,7 +69,7 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-          days={days}
+          // days={days}
           day={day}
           setDay={setDay}
           />
