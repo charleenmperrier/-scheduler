c74313dd4c232928175ba72ead8fdfcf3faf2abc
charleenmperrier <charlee_perrier@hotmail.com>
2021-03-16 21:22:36 -0700

    added appointments with hardcoded data

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 
 import "components/Application.scss";
 import DayList from "components/DayList.js"
+import Appointment from "components/Appointment"
 
 const days = [
   {
@@ -22,11 +23,49 @@ const days = [
   },
 ];
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Mike Strong",
+      interviewer: {
+        id: 2,
+        name: "Sven Jones",
+        avatar: "https://i.imgur.com/twYrpay.jpg",
+      }
+    }
+  }
+  
+];
+
 export default function Application(props) {
 
   const [day, setDay] = useState("Monday");
 
-  
+  const appointmentList = appointments.map((appointment) => {
+    return <Appointment key={appointment.id} {...appointment}/>
+  })
 
   return (
     <main className="layout">
@@ -51,7 +90,8 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointmentList}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
