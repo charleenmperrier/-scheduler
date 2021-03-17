949889f58a6ee7831fa392e36eee3d9717210a41
charleenmperrier <charlee_perrier@hotmail.com>
2021-03-16 20:47:01 -0700

    bringing the components together

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,13 +1,22 @@
 import React from "react";
 import 'components/Appointment/styles.scss'
+import Header from 'components/Appointment/Header'
+import Show from 'components/Appointment/Show'
+import Empty from 'components/Appointment/Empty'
 // import classnames from 'classnames'
 
 
 export default function Appointment(props) {
 
+  
+
   return (
     <article className='appointment'>
-     
+      <Header time={props.time}/>
+      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty />}
     </article>
   );
 }
+
+
+
