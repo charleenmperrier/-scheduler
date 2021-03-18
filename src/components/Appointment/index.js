7da07d7c5a5847e4a01fef601e6282d464f3de11
charleenmperrier <charlee_perrier@hotmail.com>
2021-03-18 15:25:29 -0700

    tests for custom hook

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,6 +3,7 @@ import 'components/Appointment/styles.scss'
 import Header from 'components/Appointment/Header'
 import Show from 'components/Appointment/Show'
 import Empty from 'components/Appointment/Empty'
+import useVisualMode from 'com'
 // import classnames from 'classnames'
 
 
---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+
+export default function useVisualMode (initial) {
+
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+
+  const transition = (newValue, replace = false) =>  {
+  
+    // push new value into history array
+    setHistory((prevHistory) => {
+      let newHistory = [...prevHistory];
+      if (replace) {
+        newHistory[newHistory.length - 1] = newValue;
+      } else {
+        newHistory.push(newValue)
+      }
+      return newHistory;
+    });
+
+    setMode(newValue)
+  };
+
+
+  const back = () => {
+  
+    setHistory((prevHistory) => {
+      
+      if (prevHistory.length <= 1) {
+        return prevHistory;
+      } else {
+
+      const lastHistory = prevHistory[prevHistory.length - 2]
+      
+      setMode(lastHistory)
+      return  prevHistory.filter((item, index) => index !== (prevHistory.length - 1))
+      }
+    })
+  }
+
+  return { mode, transition, back}
+}
