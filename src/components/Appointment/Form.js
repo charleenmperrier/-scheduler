cd1e32b11aa37a033ab40ecbc68f3c7abc3e7f9a
charleenmperrier <charlee_perrier@hotmail.com>
2021-03-22 15:44:17 -0700

    error for no name entered

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,6 +6,8 @@ export default function Form(props) {
 
   const [name, setName] = useState(props.name || '');
 
+  const [error, setError] = useState('');
+
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
   
   const reset = () => {
@@ -18,6 +20,13 @@ export default function Form(props) {
     props.onCancel();
   }
 
+  function validate() {
+    if (name === '') {
+      setError("Student name cannot be blank")
+      return;
+    }
+    props.onSave(name, interviewer);
+  }
 
   return (
     <main className="appointment__card appointment__card--create">
@@ -31,8 +40,10 @@ export default function Form(props) {
             value={name}
             onChange={(event) => setName(event.target.value)}
             data-testid="student-name-input"
+            // onSave={onSave}
             onSubmit={event => event.preventDefault()}
           />
+          <section className='appointment__validation'>{error}</section>
         </form>
         <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={(event) => setInterviewer(event)} />
       
@@ -40,7 +51,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={() => props.onSave(name, interviewer)}>Save</Button>
+          <Button confirm onClick={validate}>Save</Button>
         </section>
       </section>
     </main>
--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -49,7 +49,7 @@ export default function useApplicationData () {
     ]).then((all) => {
       setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
     });
-  }, [])
+  })
 
   return { bookInterview, cancelInterview, state, setDay }
 }
